8193fcfc247691cff16a0e534bda6277ee8cd20d
Sam Minnee <sam@silverstripe.com>
2015-12-08 17:55:02 +1300

    MINOR: Add test for IntercomScriptTags disabling

--- tests/RequestFilterTest.php
+++ tests/RequestFilterTest.php
@@ -3,6 +3,7 @@
 namespace Sminnee\SilverStripeIntercom\Tests;
 
 use Sminnee\SilverStripeIntercom\RequestFilter;
+use Sminnee\SilverStripeIntercom\IntercomScriptTags;
 
 use SapphireTest;
 use SS_HTTPRequest;
@@ -13,23 +14,31 @@ use Session;
 class RequestFilterTest extends SapphireTest {
 
 	/**
+	 * Keep track of some global state to restore in tearDown
+	 */
+	protected $saved;
+
+	function setUp() {
+		$this->saved = array(
+			'script_tags_enabled' => IntercomScriptTags::config()->enabled,
+		);
+	}
+
+	function tearDown() {
+		IntercomScriptTags::config()->enabled = $this->saved['script_tags_enabled'];
+	}
+
+	/**
 	 * @dataProvider sampleResponses
 	 */
 	function testScriptInsertion($response, $match) {
-		// Test stub
-		$request = new SS_HTTPRequest("GET", "/");
-		$model = new DataModel();
-		$session = new Session(array());
-
-		// Execute the filter
-		$filter = new RequestFilter();
-		$filter->preRequest($request, $session, $model);
-		$filter->postRequest($request, $response, $model);
-
 		// Check that script has been added before the body
 		$this->assertEquals(
 			$match,
-			preg_match('/<script>.*window.intercomSettings *= *.*<\/body>/is', $response->getBody())
+			preg_match(
+				'/<script>.*window.intercomSettings *= *.*<\/body>/is',
+				$this->checkFilterForResponse($response)->getBody()
+			)
 		);
 	}
 
@@ -49,4 +58,42 @@ class RequestFilterTest extends SapphireTest {
 
 		return $test;
 	}
+
+	/**
+	 * Test that no script is addded if IntercomScriptTags is disabled
+	 */
+	function testScriptTagsDisabling() {
+		IntercomScriptTags::config()->enabled = false;
+
+		$response = new SS_HTTPResponse("<html><head></head><body><p>regular response has script added</p></body></html>");
+
+		// Check that script has been added before the body
+		$this->assertEquals(
+			0,
+			preg_match(
+				'/<script>.*window.intercomSettings *= *.*<\/body>/is',
+				$this->checkFilterForResponse($response)->getBody()
+			)
+		);
+	}
+
+	/**
+	 * Set up test scaffold to check the RequestFilter's effect on a response
+	 */
+	function checkFilterForResponse($response) {
+		// Test stub
+		$request = new SS_HTTPRequest("GET", "/");
+		$model = new DataModel();
+		$session = new Session(array());
+
+		// Execute the filter
+		$filter = new RequestFilter();
+		$filter->preRequest($request, $session, $model);
+		$filter->postRequest($request, $response, $model);
+
+		return $response;
+	}
+
+
+
 }
